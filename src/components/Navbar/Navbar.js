9f21655957f28cb9365ba9dcbe7b1c4eaf7e1079
Yana Ilieva <yai@softwareag.com>
2022-09-02 18:30:34 +0300

    remove absolute path for logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import "./Navbar.css";
 import { useLogout } from "../../hooks/useLogout";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import logo from "../../assets/green-initiative-logo.png";
 
 export default function Navbar() {
   const { logout } = useLogout();
@@ -11,11 +12,7 @@ export default function Navbar() {
     <header>
       <div className="navbar-upper-line"></div>
       <nav className="navbar">
-        <img
-          class="logo"
-          src={require("C:\\ssev\\SAGaton\\GreenInitiative\\src\\assets\\green-initiative-logo.png")}
-          alt="logo"
-        />
+        <img class="logo" src={logo} alt="logo" />
         {user && <p>Hello, {user.displayName}!</p>}
 
         <ul>

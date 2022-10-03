bbbac575067c35aa3d6aa00da1cce52081281f51
gge <gge@softwareag.com>
2022-10-03 16:50:15 +0300

    Upload logic

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,9 +1,78 @@
 import "./Profile.css";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useState } from "react";
+import { projectStorage } from "../../firebase/config";
 
-export default function Profile() {
+const Profile = () => {
+  const { user } = useAuthContext();
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState("");
+
+  return <ReactFirebaseFileUpload />;
+};
+
+const ReactFirebaseFileUpload = () => {
+  const [image, setImage] = useState(null);
+  const [url, setUrl] = useState("");
+  const [progress, setProgress] = useState(0);
+
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      console.log("We are at handleChange");
+      console.log(e.target.files[0].name);
+      setImage(e.target.files[0]);
+      console.log("CUSTOM LOG ", image);
+    }
+  };
+
+  const handleUpload = () => {
+    console.log("aspodkapsodk " + image.name);
+    const uploadPath = "profileImage/" + image.name;
+    const uploadTask = projectStorage.ref(uploadPath).put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        projectStorage
+          .ref("profileImage")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            setUrl(url);
+          });
+      }
+    );
+  };
+
+  console.log("image: ", image);
+
   return (
     <div>
-      <div>k</div>
+      <form>
+        <progress value={progress} max="100" />
+        <br />
+        <br />
+        <input name="buton" type="file" onChange={handleChange} />
+        <button onClick={handleUpload}>Upload</button>
+        <br />
+        {url}
+        <br />
+        <img
+          src={url || "http://via.placeholder.com/300"}
+          alt="firebase-image"
+        />
+      </form>
     </div>
   );
-}
+};
+
+export default Profile;
